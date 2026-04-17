089714b26994fcc417f492eff89057cea9dbc61f
fangyuchu <fangyuchu@qq.com>
2026-04-17 19:49:05 +0800

    Simplify pause logic in model runner and check mask for ft backend in AsyncGPUModelRunnerOutput
    Signed-off-by: fangyuchu <fangyuchu@qq.com>

--- vllm/v1/worker/sentinel/gpu_worker_sentinel.py
+++ vllm/v1/worker/sentinel/gpu_worker_sentinel.py
@@ -7,7 +7,7 @@ import torch
 import zmq
 
 from vllm.config import ParallelConfig
-from vllm.distributed import get_pp_group, get_tp_group
+from vllm.distributed import get_ep_group, get_pp_group, get_tp_group
 from vllm.logger import init_logger
 from vllm.utils.network_utils import close_sockets, make_zmq_socket
 from vllm.v1.fault_tolerance import BaseSentinel
@@ -15,12 +15,18 @@ from vllm.v1.fault_tolerance.utils import FaultToleranceRequest, FaultToleranceR
 
 logger = init_logger(__name__)
 
+_GLOBAL_PAUSE_EVENT = threading.Event()
+
+
+def get_pause_event() -> threading.Event:
+    global _GLOBAL_PAUSE_EVENT
+    return _GLOBAL_PAUSE_EVENT
+
 
 class WorkerSentinel(BaseSentinel):
     def __init__(
         self,
         parallel_config: ParallelConfig,
-        pause_event: threading.Event,
         device: torch.device,
         worker_cmd_addr: str,
     ):
@@ -30,7 +36,6 @@ class WorkerSentinel(BaseSentinel):
         identity_str = f"PP{pp_rank}_TP{tp_rank}"
         super().__init__(f"{dp_rank}_{identity_str}", identity_str.encode())
         self.device = device
-        self.pause_event = pause_event
         torch.accelerator.set_device_index(self.device)
 
         self.engine_core_cmd_socket = make_zmq_socket(
@@ -40,6 +45,18 @@ class WorkerSentinel(BaseSentinel):
             bind=False,
             identity=self.identity,
         )
+
+        # Currently, only deepep_ll and nixl_ep backends support fault tolerance.
+        ft_backend_set = {"deepep_low_latency", "nixl_ep"}
+        self.use_ft_backend = (
+            parallel_config.all2all_backend in ft_backend_set
+            and parallel_config.data_parallel_size > 1
+        )
+        if self.use_ft_backend:
+            world_size = get_ep_group().world_size
+            self.mask = torch.zeros((world_size,), device=self.device, dtype=torch.int)
+            # todo: last_mask is prepared and should be updated in scaling down.
+            self.last_mask = torch.zeros_like(self.mask)
 
         threading.Thread(
             target=self.run, daemon=True, name="WorkerSentinelThread"
@@ -66,7 +83,7 @@ class WorkerSentinel(BaseSentinel):
             self.sentinel_dead = True
 
     def pause(self, ft_request: FaultToleranceRequest) -> FaultToleranceResult:
-        self.pause_event.set()
+        get_pause_event().set()
         return FaultToleranceResult(ft_request.request_id, True)
 
     def shutdown(self):
