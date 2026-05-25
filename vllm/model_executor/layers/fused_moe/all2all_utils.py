d7f703779cb196ad059bb4f7fd33c034e63e01ad
fangyuchu <fangyuchu@qq.com>
2026-05-25 21:30:28 +0800

    add fault detection for nixl_ep and deepep_ll
    Signed-off-by: fangyuchu <fangyuchu@qq.com>

--- vllm/model_executor/layers/fused_moe/all2all_utils.py
+++ vllm/model_executor/layers/fused_moe/all2all_utils.py
@@ -48,6 +48,21 @@ if current_platform.is_cuda_alike():
             NixlEPPrepareAndFinalize,
         )
 
+def get_ep_all2all_manager(eep_stage: bool = False) -> Any:
+    if eep_stage:
+        from vllm.distributed.elastic_ep.standby_state import get_standby_ep_group
+
+        ep_group = get_standby_ep_group()
+        assert ep_group is not None
+        device_communicator = ep_group.device_communicator
+    else:
+        device_communicator = get_ep_group().device_communicator
+
+    assert device_communicator is not None
+    all2all_manager = device_communicator.all2all_manager
+    assert all2all_manager is not None
+    return all2all_manager
+
 
 def _get_ep_all2all_manager(eep_stage: bool = False) -> Any:
     if eep_stage:
@@ -134,7 +149,7 @@ def maybe_make_prepare_finalize(
                 "Detected DP deployment with no --enable-expert-parallel. "
                 "Falling back to AllGather+ReduceScatter dispatch/combine."
             )
-            all2all_manager = _get_ep_all2all_manager(eep_stage)
+            all2all_manager = get_ep_all2all_manager(eep_stage)
             return make_moe_prepare_and_finalize_naive_dp_ep(
                 is_sequence_parallel=moe.moe_parallel_config.is_sequence_parallel,
                 num_dispatchers=all2all_manager.world_size,
@@ -143,7 +158,7 @@ def maybe_make_prepare_finalize(
         else:
             return make_moe_prepare_and_finalize_no_dp_ep(use_monolithic)
 
-    all2all_manager = _get_ep_all2all_manager(eep_stage)
+    all2all_manager = get_ep_all2all_manager(eep_stage)
 
     prepare_finalize: FusedMoEPrepareAndFinalize | None = None
 
