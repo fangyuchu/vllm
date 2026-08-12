658b4e2a7be1804d6e1286fa7772d4f442fb2787
fangyuchu <fangyuchu@qq.com>
2026-08-12 16:10:41 +0800

    add tp barrier and avoid the exit of eplb async thread of fault (#265)
    * avoid the exit of eplb async thread on fault.
    
    Signed-off-by: fangyuchu <fangyuchu@qq.com>
    
    * add tp barrier
    
    Signed-off-by: fangyuchu <fangyuchu@qq.com>
    
    ---------
    
    Signed-off-by: fangyuchu <fangyuchu@qq.com>

--- vllm/v1/worker/gpu/spec_decode/autoregressive/speculator.py
+++ vllm/v1/worker/gpu/spec_decode/autoregressive/speculator.py
@@ -213,6 +213,7 @@ class AutoRegressiveSpeculator(DraftModelSpeculator):
             dp_size=self.dp_size,
             dp_rank=self.dp_rank,
             need_eager=is_profile,
+            parallel_config=self.vllm_config.parallel_config,
         )
 
         self._prepare_eplb_forward(input_batch.num_tokens)
@@ -260,6 +261,7 @@ class AutoRegressiveSpeculator(DraftModelSpeculator):
             dp_size=self.dp_size,
             dp_rank=self.dp_rank,
             need_eager=is_profile,
+            parallel_config=self.vllm_config.parallel_config,
         )
 
         # Generate the remaining num_speculative_steps - 1 draft tokens.
